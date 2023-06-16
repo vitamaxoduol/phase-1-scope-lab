5901ef822359160519e5c20ae13fec477520c76d
Vitamax Oduol <85936735+vitamaxoduol@users.noreply.github.com>
2023-06-16 20:28:44 +0300

    Completed assignmemt

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = 'bob';
+function customerName() {
+    return 'bob';
+}
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  return 'BOB'
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+
+const leastFavoriteCustomer = 'maybe not bob'; //global scope declation
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'that is bob' //attempt to make a new reassignmemt
+}
